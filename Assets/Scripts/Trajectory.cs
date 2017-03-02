8686d1a1645977f2ca9318a1e1acf26ef9e0eed5
Ross Luo <ross.s.luo@gmail.com>
2017-03-02 15:16:32 -0800

    Updated with latest version of trajectory script

--- Assets/Scripts/Trajectory.cs
+++ Assets/Scripts/Trajectory.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Trajectory : MonoBehaviour {
+public class Trajectory : MonoBehaviour
+{
 
     // Use this for initialization
     //public Cannon_Vertical_CS cannonScript;
@@ -13,36 +14,59 @@ public class Trajectory : MonoBehaviour {
     private Vector3 position;
     private Vector3 velocity;
 
-    
-    void Start() {
+    byte current_player = 2; // Default to 2 so that laser is off by default
+    GameObject n_manager;
+    network_manager n_manager_script;
+
+
+
+    void Start()
+    {
         trajectoryLine = GetComponent<LineRenderer>();
         trajectoryLine.startColor = Color.red;
         trajectoryLine.endColor = Color.red;
-        position = bulletScript.transform.position + -3* bulletScript.transform.forward;
+        position = bulletScript.transform.position + -3 * bulletScript.transform.forward;
         velocity = bulletScript.transform.forward * bulletScript.Bullet_Force; //Offset added in original script-What does it do?
-
+        //trajectoryLine.enabled = false;
 
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update()
+    {
         position = bulletScript.transform.position + -3 * bulletScript.transform.forward;
         velocity = bulletScript.transform.forward * bulletScript.Bullet_Force;
         updateTrajectory(position, velocity);
+        // Use hotkey "t" to toggle whether the laser sight is displayed or not
+        if (Input.GetKeyDown("t") == true)
+        {
+            trajectoryLine.enabled = !trajectoryLine.enabled;
+        }
     }
 
-    private void updateTrajectory(Vector3 position, Vector3 velocity) {
+    private void updateTrajectory(Vector3 position, Vector3 velocity)
+    {
         Vector3 gravity = new Vector3(0, -9.81f, 0);
         int numSteps = 200;
-        //float timeDelta = 50.0f / velocity.magnitude;
-        float timeDelta = 0.1f;
+        //float timeDelta = 2.0f / velocity.magnitude;
+        float timeDelta = .1f;
         trajectoryLine.numPositions = numSteps;
-        for (int i = 0; i < numSteps; ++i) {
+        for (int i = 0; i < numSteps; ++i)
+        {
             trajectoryLine.SetPosition(i, position);
             position += velocity * timeDelta + 0.5f * gravity * timeDelta * timeDelta;
             velocity += gravity * timeDelta;
 
         }
 
+    }
+
+    public void Prep()
+    {
+        n_manager = GameObject.Find("Custom Network Manager(Clone)");
+        n_manager_script = n_manager.GetComponent<network_manager>();
+        current_player = (byte)(n_manager_script.client_players_amount);
+
+        trajectoryLine.enabled = current_player == 1;
     }
 }
