33fc11e4aba726886ff1c0fad44bc0fd95efcf2a
xuanyuzhou98 <steven@airbears2-10-142-37-207.airbears2.1918.berkeley.edu>
2017-03-19 16:37:51 -0700

    angle display

---
+++ Assets/Angle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Angle : MonoBehaviour {
+	Text myAngle;
+	private int upAngle;
+	private int rightAngle;
+	GameObject myBarral;
+	// Use this for initialization
+	void Start () {
+		myAngle = GetComponent<Text>();
+		myBarral = GameObject.Find ("Barrel_Base");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		upAngle = - (int) myBarral.GetComponent<Transform> ().localEulerAngles.x;
+		rightAngle = (int) myBarral.GetComponent<Transform> ().localEulerAngles.y;
+		if (upAngle < -250) {
+			upAngle = upAngle + 360;
+		}
+		if (rightAngle > 250) {
+			rightAngle = rightAngle - 360;
+		}
+		myAngle.text = upAngle.ToString () + "    " + rightAngle.ToString ();
+
+	}
+}
